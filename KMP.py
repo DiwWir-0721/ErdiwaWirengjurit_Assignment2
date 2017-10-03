6b4221aeb0028ce3d34839b2c5a1cd2d473261bb
DiwWir-0721 <32050642+DiwWir-0721@users.noreply.github.com>
2017-10-03 08:28:21 +0700

    Add files via upload

---
+++ KMP.py
@@ -0,0 +1,13 @@
+all = []
+a = input("The word: ")
+b = input("The second word: ")
+c = input("The third word: ")
+print(a[0],b[0],c[0])
+
+d = input("The word: ")
+e = input("The second word: ")
+print(d[0],e[0])
+
+f = input("The word: ")
+g = input("The second word: ")
+print(f[0],g[0])
